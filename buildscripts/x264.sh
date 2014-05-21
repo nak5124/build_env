1dbece773a48283e74c93e872ebbc6146c50ca2f
Yuta NAKAI <nak5124@live.jp>
2014-05-21 20:29:59 +0900

    Add x264 build scripts.

---
+++ buildscripts/x264.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+
+LOGS_DIR=${HOME}/log/x264
+if [ ! -d $LOGS_DIR ] ; then
+    mkdir -p $LOGS_DIR
+fi
+
+build_x264() {
+    clear; echo "Build x264"
+
+    if [ ! -d ${HOME}/x264 ] ; then
+        cd $HOME
+        git clone git://github.com/nak5124/x264.git
+    fi
+    cd ${HOME}/x264
+
+    git clean -fdx > /dev/null 2>&1
+    git reset --hard > /dev/null 2>&1
+    git pull --rebase > /dev/null 2>&1
+    git_hash > ${LOGS_DIR}/x264.hash
+    git_rev >> ${LOGS_DIR}/x264.hash
+
+    local PKG_DIR=/usr/x264_libs/lib/pkgconfig
+    local SAMPLE_FILES="${HOME}/y4m/deadline_cif.y4m ${HOME}/y4m/foreman_cif.y4m ${HOME}/y4m/soccer_4cif.y4m"
+    local patches_num=22
+
+    source cpath x86_64
+    for bitdepth in 8 10
+    do
+        for buildtype in all lite
+        do
+            if [ "$buildtype" == "all" ] ; then
+                local _format="all"
+                local _opt=""
+                if [ "$bitdepth" -eq 8 ] ; then
+                    local _clopt=""
+                    local _target="Full 8bit"
+                else
+                    local _clopt="--disable-opencl"
+                    local _target="Full 10bit"
+                fi
+            else
+                local _format="420"
+                local _opt="--disable-interlaced --disable-swscale --disable-lavf"
+                local _clopt="--disable-opencl"
+                if [ "$bitdepth" -eq 8 ] ; then
+                    local _target="Lite 8bit"
+                else
+                    local _target="Lite 10bit"
+                fi
+            fi
+
+            echo "===> configure x264 ${_target}"
+            PKG_CONFIG_PATH=$PKG_DIR             \
+            ./configure ${_clopt}                \
+                        --enable-win32thread     \
+                        --bit-depth=$bitdepth    \
+                        --chroma-format=$_format \
+                        --enable-strip           \
+                        ${_opt}                  \
+            > ${LOGS_DIR}/x264_config_${bitdepth}bit_${buildtype}.log 2>&1 || exit 1
+            echo "done"
+
+            make clean > /dev/null 2>&1
+            echo "===> make fprofiled x264 ${_target}"
+            make -j9 -O fprofiled VIDS="$SAMPLE_FILES" \
+                > ${LOGS_DIR}/x264_make_${bitdepth}bit_${buildtype}.log 2>&1 || exit 1
+            echo "done"
+
+            if [ "$bitdepth" -eq 8 ] && [ "$buildtype" == "all" ] ; then
+                local X264_REVISION=`x264 --version|grep "x264"|awk '{print $2}'|awk -F . '{print $3}'|awk -F + '{print $1}'`
+                local DEST_DIR=/usr/local/x264/x264_r${X264_REVISION}_x64_v2
+                if [ ! -d ${DEST_DIR}/lite ] ; then
+                    mkdir -p ${DEST_DIR}/lite
+                fi
+            fi
+
+            echo "===> copy x264 ${_target}"
+            if [ "$bitdepth" -eq 8 ] ; then
+                if [ "$buildtype" == "all" ] ; then
+                    cp -fa ${HOME}/x264/x264.exe ${DEST_DIR}/x264_x64.exe
+                    ${HOME}/x264/x264.exe --fullhelp > ${DEST_DIR}/x264_x64_help.txt
+                else
+                    cp -fa ${HOME}/x264/x264.exe ${DEST_DIR}/lite/x264_x64_lite.exe
+                    ${HOME}/x264/x264.exe --fullhelp > ${DEST_DIR}/lite/x264_x64_lite_help.txt
+                fi
+            else
+                if [ "$buildtype" == "all" ] ; then
+                    cp -fa ${HOME}/x264/x264.exe ${DEST_DIR}/x264_x64-10bit.exe
+                    ${HOME}/x264/x264.exe --fullhelp > ${DEST_DIR}/x264_x64-10bit_help.txt
+                else
+                    cp -fa ${HOME}/x264/x264.exe ${DEST_DIR}/lite/x264_x64_lite-10bit.exe
+                    ${HOME}/x264/x264.exe --fullhelp > ${DEST_DIR}/lite/x264_x64_lite-10bit_help.txt
+                fi
+            fi
+            echo "done"
+            make distclean > /dev/null 2>&1
+        done
+    done
+
+    git archive --format=tar --prefix=x264_r${X264_REVISION}_src/ HEAD | lzip -9 -o /usr/local/x264/x264_r${X264_REVISION}_src.tar
+    git format-patch HEAD~$patches_num -o x264_r${X264_REVISION}_patches
+    tar cf - x264_r${X264_REVISION}_patches | lzip -9 -o /usr/local/x264/x264_r${X264_REVISION}_patches.tar -
+    cp -fa /usr/local/x264/COPYING.GPLv3 $DEST_DIR
+    cp -fa /usr/local/x264/COPYING.GPLv2 ${DEST_DIR}/lite
+
+    cd $HOME
+    return 0
+}
+
+build_x264
+
+clear; echo "Everything has been successfully completed!"
---
+++ buildscripts/x264_libs.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+
+PREFIX_DIR=/usr/x264_libs
+PATCHES_DIR=${HOME}/patches/ffmpeg
+LOGS_DIR=${HOME}/log/x264_libs
+if [ ! -d $LOGS_DIR ] ; then
+    mkdir -p $LOGS_DIR
+fi
+
+# FFmpeg
+patch_ffmpeg() {
+    echo "===> patching... FFmpeg"
+
+    local _N=67
+    for i in `seq 1 ${_N}`
+    do
+        local num=$(printf "%04d" $i )
+        if [ "$i" == 1 ] ; then
+            patch -p1 < ${PATCHES_DIR}/${num}*.patch \
+                > ${LOGS_DIR}/ffmpeg_patches.log 2>&1 || exit 1
+        else
+            patch -p1 < ${PATCHES_DIR}/${num}*.patch \
+                >> ${LOGS_DIR}/ffmpeg_patches.log 2>&1 || exit 1
+        fi
+    done
+
+    echo "done"
+
+    return 0
+}
+
+build_ffmpeg() {
+    clear; echo "Build libav{codec,format,util},libswscale git-master"
+
+    if [ ! ${HOME}/FFmpeg ] ; then
+        cd $HOME
+        git clone git://source.ffmpeg.org/ffmpeg.git FFmpeg
+    fi
+    cd ${HOME}/FFmpeg
+
+    git clean -fdx > /dev/null 2>&1
+    git reset --hard > /dev/null 2>&1
+    git pull
+    git_hash > ${LOGS_DIR}/ffmpeg.hash
+    git_rev >> ${LOGS_DIR}/ffmpeg.hash
+
+    patch_ffmpeg
+
+    local _EXCFLAGS="-I${PREFIX_DIR}/include -fexcess-precision=fast"
+    local _EXLDFLAGS="-L${PREFIX_DIR}/lib"
+
+    source cpath x86_64
+    echo "===> configure libav{codec,format,util},libswscal"
+    PKG_CONFIG_PATH=${PREFIX_DIR}/lib/pkgconfig         \
+    ./configure --prefix=$PREFIX_DIR                    \
+                --enable-gpl --enable-version3          \
+                --disable-programs --disable-doc        \
+                --disable-avdevice --disable-swresample \
+                --disable-postproc --disable-avfilter   \
+                --disable-avresample --disable-network  \
+                --disable-encoders                      \
+                --disable-hwaccels                      \
+                --disable-muxers                        \
+                --disable-demuxer=matroska_haali        \
+                --disable-devices --disable-filters     \
+                --disable-iconv                         \
+                --arch=x86_64                           \
+                --disable-debug                         \
+                --extra-cflags="${_EXCFLAGS}"           \
+                --extra-ldflags="${_EXLDFLAGS}"         \
+    > ${LOGS_DIR}/ffmpeg_config.log 2>&1 || exit 1
+    echo "done"
+
+    make clean > /dev/null 2>&1
+    echo "===> make libav{codec,format,util},libswscale"
+    make -j9 -O > ${LOGS_DIR}/ffmpeg_make.log 2>&1 || exit 1
+    echo "done"
+    echo "===> install libav{codec,format,util},libswscale ${arch}"
+    make install > ${LOGS_DIR}/ffmpeg_install.log 2>&1 || exit 1
+    echo "done"
+    make distclean > /dev/null 2>&1
+
+    return 0
+}
+
+# L-SMASH
+build_liblsmash() {
+    clear; echo "Build liblsmash git-master"
+
+    if [ ! -d ${HOME}/L-SMASH ] ; then
+        cd $HOME
+        git clone https://github.com/l-smash/l-smash.git L-SMASH
+    fi
+    cd ${HOME}/L-SMASH
+
+    git clean -fdx > /dev/null 2>&1
+    git reset --hard > /dev/null 2>&1
+    git pull
+    git_hash > ${LOGS_DIR}/lsmash.hash
+    git_rev >> ${LOGS_DIR}/lsmash.hash
+
+    source cpath x86_64
+    echo "===> configure liblsmash"
+    ./configure --prefix=$PREFIX_DIR \
+        > ${LOGS_DIR}/lsmash_config.log 2>&1 || exit 1
+    echo "done"
+
+    make clean > /dev/null 2>&1
+    echo "===> make liblsmash"
+    make lib -j5 -O > ${LOGS_DIR}/lsmash_make.log 2>&1 || exit 1
+    echo "done"
+    echo "===> install liblsmash"
+    make install-lib > ${LOGS_DIR}/lsmash_install.log 2>&1 || exit 1
+    echo "done"
+    make distclean > /dev/null 2>&1
+
+    return 0
+}
+
+build_ffmpeg
+build_liblsmash
+
+clear; echo "Everything has been successfully completed!"
+
