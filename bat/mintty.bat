3eb2f6f8b414f3a1d69879dfac015f2ee670e518
Yuta NAKAI <nak5124@live.jp>
2014-06-12 22:56:21 +0900

    mintty.bat: Change default MSYSTEM.

--- bat/mintty.bat
+++ bat/mintty.bat
@@ -1,7 +1,7 @@
 @echo off
-set MSYSTEM=MINGW32
+set MSYSTEM=MINGW64
 set WD=%~dp0\bin\
 set MSYSCON=mintty.exe
 REM set CHERE_INVOKING=yes
-start %WD%mintty -c ~/.minttyrc -p -1020,0 -i /msys.ico -
+start %WD%mintty -c ~/.minttyrc -p -1020,0 -i /msys.ico /bin/bash --login
 exit
--- bat/mintty_exp.bat
+++ bat/mintty_exp.bat
@@ -1,7 +1,7 @@
 @echo off
-set MSYSTEM=MINGW32
+set MSYSTEM=MINGW64
 set WD=C:\msys2\bin\
 set MSYSCON=mintty.exe
 set CHERE_INVOKING=yes
-start /d %1 /B %WD%mintty -p -1020,0 -i /msys.ico -
+start /d %1 /B %WD%mintty -p -1020,0 -i /msys.ico /bin/bash --login
 exit
