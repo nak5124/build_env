bcbd543d7568af6d34724fa013c300d5cd38c75d
Yuta NAKAI <nak5124@live.jp>
2014-06-29 12:24:23 +0900

    mintty_exp.bat: Fix failed login attempt.

--- bat/mintty_exp.bat
+++ bat/mintty_exp.bat
@@ -4,5 +4,5 @@ set WD=C:\msys2\usr\bin\
 set MSYSCON=mintty.exe
 set MSYS=winsymlinks:nativestrict
 set CHERE_INVOKING=yes
-start /d %1 /B %WD%mintty -p -1020,0 -i /msys2.ico /usr/bin/bash --login %*
+start /d %1 /B %WD%mintty -p -1020,0 -i /msys2.ico /usr/bin/bash --login
 exit
