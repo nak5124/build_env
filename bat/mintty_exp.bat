9067d2f96f3ea376cfbbad19ae10b70a4ea8e08b
nak5124 <nak5124@live.jp>
2015-03-10 15:33:31 +0900

    Fix mintty_exp.bat.
    Workaround for Win8.1 systems enabled UAC.

--- bat/mintty_exp.bat
+++ bat/mintty_exp.bat
@@ -1,7 +1,9 @@
 @echo off
 set MSYSTEM=MINGW64
-set WD=C:\msys2\usr\bin\
+set WD=C:\\\\msys2\\\\usr\\\\bin\\\\
 set MSYSCON=mintty
 set CHERE_INVOKING=yes
-start /d %1 /B %WD%%MSYSCON% -c ~/.minttyrc -p -1220,80 -i /msys2.ico /usr/bin/bash --login
+start /d %1 /B %WD%%MSYSCON% -c ~/.minttyrc -i /msys2.ico /usr/bin/bash -c ^
+    "export WD=%WD%; export MSYSCON=%MSYSCON%; export CHERE_INVOKING=%CHERE_INVOKING%; exec /usr/bin/bash --login"
+REM start /d %1 /B %WD%%MSYSCON% -c ~/.minttyrc -p -1220,80 -i /msys2.ico /usr/bin/bash --login
 exit
