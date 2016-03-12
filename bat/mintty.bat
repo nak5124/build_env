974705e2e765a101571f4bbdf6d2a5a762f880e3
Yuta Nakai <nak5124@live.jp>
2016-03-12 22:44:00 +0900

    Update for new PC env.

--- bat/mintty.bat
+++ bat/mintty.bat
@@ -2,5 +2,5 @@
 set MSYSTEM=MINGW64
 set WD=%~dp0usr\bin\
 set MSYSCON=mintty
-start %WD%%MSYSCON% -c ~/.minttyrc -p -1220,80 -i /msys2.ico /usr/bin/bash --login %*
+start %WD%%MSYSCON% -c ~/.minttyrc -i /msys2.ico /usr/bin/bash --login %*
 exit
--- bat/mintty_exp.bat
+++ bat/mintty_exp.bat
@@ -1,6 +1,6 @@
 @echo off
 set MSYSTEM=MINGW64
-set WD=C:\\\\msys2\\\\usr\\\\bin\\\\
+set WD=E:\\\\msys2\\\\usr\\\\bin\\\\
 set MSYSCON=mintty
 set CHERE_INVOKING=yes
 start /d %1 /B %WD%%MSYSCON% -c ~/.minttyrc -i /msys2.ico /usr/bin/bash -c ^
