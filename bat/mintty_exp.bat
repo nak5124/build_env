a4a632f112cc69785d2eae82ca16b580cc7af7a0
Yuta Nakai <nak5124@live.jp>
2016-03-21 02:12:17 +0900

    bat/mintty_exp.bat: Export MSYSTEM.

--- bat/mintty_exp.bat
+++ bat/mintty_exp.bat
@@ -4,6 +4,6 @@ set WD=E:\\\\msys2\\\\usr\\\\bin\\\\
 set MSYSCON=mintty
 set CHERE_INVOKING=yes
 start /d %1 /B %WD%%MSYSCON% -c ~/.minttyrc -i /msys2.ico /usr/bin/bash -c ^
-    "export WD=%WD%; export MSYSCON=%MSYSCON%; export CHERE_INVOKING=%CHERE_INVOKING%; exec /usr/bin/bash --login"
+    "export WD=%WD%; export MSYSCON=%MSYSCON%; export CHERE_INVOKING=%CHERE_INVOKING%; export MSYSTEM=%MSYSTEM%; exec /usr/bin/bash --login"
 REM start /d %1 /B %WD%%MSYSCON% -c ~/.minttyrc -p -1220,80 -i /msys2.ico /usr/bin/bash --login
 exit
